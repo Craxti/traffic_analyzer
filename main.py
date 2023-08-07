e2d6ff9ad55773b00d5275cae2d199b6ef029d65
Aleksandr Fetisov <alexgette17@gmail.com>
2023-08-07 13:51:17 +0400

    upd

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from traffic_analyzer.capture import capture_traffic
 from traffic_analyzer.analyze import analyze_traffic
 from traffic_analyzer.visualize import update_visualizations
 
+
 def setup_logger():
     logging.basicConfig(
         level=logging.INFO,
@@ -16,19 +17,23 @@ def setup_logger():
         ]
     )
 
+
 def get_default_interface():
     return psutil.net_io_counters(pernic=True).keys().__iter__().__next__()
+
 
 def data_processing(interface, packet_count, queue):
     packets = capture_traffic(interface, packet_count)
     results = analyze_traffic(packets)
     queue.put(results)
 
+
 def real_time_visualization(queue):
     while True:
         results = queue.get()
         update_visualizations(results)
         time.sleep(5)
+
 
 def main():
     setup_logger()
@@ -46,6 +51,7 @@ def main():
 
     process_data.join()
     process_viz.join()
+
 
 if __name__ == "__main__":
     main()
