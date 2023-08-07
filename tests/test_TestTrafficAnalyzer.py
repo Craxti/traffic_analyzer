91350be06b2c2cbfd7383729f1bd20522334a01e
Aleksandr Fetisov <alexgette17@gmail.com>
2023-08-07 12:59:24 +0400

    upd analyze

--- tests/test_TestTrafficAnalyzer.py
+++ tests/test_TestTrafficAnalyzer.py
@@ -1,7 +1,10 @@
+from scapy.layers.inet import IP, TCP
 import unittest
 from unittest.mock import patch
 from scapy.packet import Packet
 from traffic_analyzer.capture import capture_traffic
+from traffic_analyzer.analyze import detect_port_scan, detect_malicious_payload
+
 
 
 class TestTrafficAnalyzer(unittest.TestCase):
@@ -16,6 +19,30 @@ class TestTrafficAnalyzer(unittest.TestCase):
 
         self.assertEqual(len(packets), 1)
         self.assertEqual(packets[0], mock_packet)
+
+    def test_detect_port_scan(self):
+        packets = [
+            IP(src="192.168.0.1") / TCP(flags=2),  # Port scan packet (SYN flag set)
+            IP(src="192.168.0.2") / TCP(flags=16),  # Not a port scan packet (ACK flag set)
+            IP(src="192.168.0.3") / TCP(flags=2),  # Port scan packet (SYN flag set)
+            IP(src="192.168.0.1") / TCP(flags=8),  # Not a port scan packet (PSH flag set)
+        ]
+
+        scan_ips = detect_port_scan(packets)
+        expected_scan_ips = ["192.168.0.1", "192.168.0.3"]
+
+        self.assertEqual(scan_ips, expected_scan_ips)
+
+    def test_detect_malicious_payload(self):
+        packets = [b'This packet contains malware', b'Another packet with malware']
+
+        malicious_packets = detect_malicious_payload(packets)
+        expected_malicious_packets = [
+            b"This packet contains malware",
+            b"Another packet with malware",
+        ]
+
+        self.assertEqual(malicious_packets, expected_malicious_packets)
 
 
 if __name__ == '__main__':
